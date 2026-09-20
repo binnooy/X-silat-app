59841ad4e77ee46ed7f63c69f906f4ce4890b6ae
vin30149@gmail.com <vin>
2026-09-20 12:30:12 +0800

    Add editable fighter information

---
+++ lib/models/fighter.dart
@@ -0,0 +1,17 @@
+class Fighter {
+  String name;
+  int age;
+  double weight;
+  String from;
+  String team;
+  String category;
+
+  Fighter({
+    required this.name,
+    required this.age,
+    required this.weight,
+    required this.from,
+    required this.team,
+    required this.category,
+  });
+}
